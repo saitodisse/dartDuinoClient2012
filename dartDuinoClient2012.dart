f6a264f0ba03e4eee7249dd7d57cdb9acc5bb05f
Marcio Camurati <marcio.camurati@oninteractive.com.br>
2012-04-22 11:56:53 -0300

    JSON request working

--- dartDuinoClient2012.dart
+++ dartDuinoClient2012.dart
@@ -1,9 +1,23 @@
 #import('dart:html');
+#import('dart:json');
 #source('Config.dart');
 
 class dartDuinoClient2012 {
 
   dartDuinoClient2012() {
+    initializeFromUrl();
+  }
+  
+  static void initializeFromUrl() {
+    XMLHttpRequest request = new XMLHttpRequest();
+    request.withCredentials = true;
+    request.on.load.add((e) { 
+      window.alert("${JSON.parse(request.responseText)}"); 
+    });
+    request.open('GET', "http://${Config.HTTP_HOST}:${Config.HTTP_PORT}/", true);
+//    request.setRequestHeader("Access-Control-Allow-Credentials", "false");
+    request.send();
+    //request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
   }
 
   void run() {
